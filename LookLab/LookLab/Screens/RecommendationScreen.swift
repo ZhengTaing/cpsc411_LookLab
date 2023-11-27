60db98d8e63110c4bad25544bd2f8f4ac49c4ff6
roymasepequena <roy.masepequena@csu.fullerton.edu>
2023-11-26 22:10:35 -0800

    Home page and recommendation page done - roy

---
+++ LookLab/LookLab/Screens/RecommendationScreen.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendationScreen.swift
+//  LookLab
+//
+//  Created by Roy Joacquin Masepequena on 11/26/23.
+//
+
+import SwiftUI
+
+struct RecommendationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecommendationScreen()
+}
---
+++ LookLab/LookLab/Screens/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  LookLab
+//
+//  Created by Roy Joacquin Masepequena on 11/25/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
