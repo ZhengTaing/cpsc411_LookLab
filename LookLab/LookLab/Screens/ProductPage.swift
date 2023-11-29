d68290e4dc926aa44a475a25489dc305d0003329
Jbraneke <mongsy2011@yahoo.com>
2023-11-28 18:45:57 -0800

    Product screens completed

---
+++ LookLab/LookLab/Screens/ProductPage.swift
@@ -0,0 +1,18 @@
+//
+//  ProductPage.swift
+//  LookLab
+//
+//  Created by Jeremy Braneke-Hite on 11/28/23.
+//
+
+import SwiftUI
+
+struct ProductPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductPage()
+}
---
+++ LookLab/LookLab/Screens/ProductScreen6.swift
@@ -0,0 +1,18 @@
+//
+//  ProductScreen6.swift
+//  LookLab
+//
+//  Created by Jeremy Braneke-Hite on 11/28/23.
+//
+
+import SwiftUI
+
+struct ProductScreen6: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductScreen6()
+}
